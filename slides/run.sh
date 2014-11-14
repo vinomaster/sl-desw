c83bc2d6d3985cd1b47033f296aa92e3bb35f231
Dan Gisolfi <vinomaster@users.noreply.github.com>
2014-11-14 14:28:46 -0500

    Update run.sh

--- slides/run.sh
+++ slides/run.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Licensed Internal Code - Property of IBM
-# Restricted Materials of IBM
-# Package: com.ibm.swget.dockersig
-# (c) Copyright IBM Corp. 2013, 2014
-# All Rights Reserved
-# US Government Users Restricted Rights - Use, duplication or disclosure
-# restricted by GSA ADP Schedule Contract with IBM Corp.
-docker run -p 8000:8000 -t -i -d `pwd`:/revealjs/md parente/revealjs
+# Process and render slideshow source in slides.md using reveal.js
+docker run -p 8000:8000 -t -i -d -v `pwd`:/revealjs/md parente/revealjs
